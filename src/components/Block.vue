e63e1e8648c7ab925bf2c20e9d61e9c511a23f96
Vaishnavi Zurange <vaishnavizurange20@gmail.com>
2021-08-12 19:30:49 +0530

    first commit upload files

---
+++ src/components/Block.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stoptimer">
+      Click me
+  </div>
+</template>
+
+<script>
+export default {
+    props:['delay'],
+    data(){
+        return{
+            showBlock: false,
+            timer: null,
+            reactiontime:0
+        }
+    },
+    mounted(){
+        setTimeout(()=>{
+            this.showBlock = true
+            this.starttimer()
+        },this.delay)
+    },
+    methods : {
+        starttimer(){
+            this.timer=setInterval(() => {
+                this.reactiontime += 10
+            }, 10);
+        },
+        stoptimer(){
+            clearInterval(this.timer)
+            this.$emit('end',this.reactiontime)
+        }
+    }
+}
+</script>
+
+<style>
+    .block{
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color:white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
